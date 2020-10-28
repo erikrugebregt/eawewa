1fdaa474e2ce700b09ffe65e39507b3312031b00
Christopher CSV <s1134223@student.windesheim.nl>
2020-10-28 15:52:10 +0100

    Animatie modal

--- js/ezweb.js
+++ js/ezweb.js
@@ -1,5 +1,66 @@
-function openModal()
+/**
+ * Accepts html for content of modal.
+ */
+async function openModal(htmlModalContent)
 {
+    $("#leftBar").show();
+    $("#rightBar").show();
+    $("#topBar").show();
+    $("#bottomBar").show();
 
+    $("#leftBar").animate(
+        {
+            "height": "50%"
+        },
+        500,
+        function ()
+        {
+            $("#bottomBar").animate({"width": "50%"}, 500, function () {});
+        }
+    );
+    $("#topBar").animate(
+        {
+            "width": "50%"
+        },
+        500,
+        function ()
+        {
+            $("#rightBar").animate(
+                {
+                    "height": "50%"
+                },
+                500,
+                function ()
+                {
+                    $("#myModal").fadeIn(1000, function ()
+                    {
+                        $("#leftBar").fadeOut("slow",   function () { $("#leftBar").css("height", "0px"); });
+                        $("#rightBar").fadeOut("slow",  function () { $("#rightBar").css("height", "0px"); });
+                        $("#topBar").fadeOut("slow",    function () { $("#topBar").css("width", "0px"); });
+                        $("#bottomBar").fadeOut("slow", function () { $("#bottomBar").css("width", "0px"); });
+                    });
+                }
+            );
+        }
+    );
 }
 
+
+$("#contactBtn").click(function ()
+{
+    openModal();
+});
+
+
+/* ------------------------------------------------- MODAL ------------------------------------------------- */
+document.getElementsByClassName("close")[0].onclick = function ()
+{
+    $("#myModal").fadeOut("slow");
+};
+window.onclick = function (event)
+{
+    if (event.target == document.getElementById("myModal"))
+    {
+        $("#myModal").fadeOut("slow");
+    }
+}
